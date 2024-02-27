1f9724ed8f33a2be09dc96be7f3e07ad4d63becf
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-02-27 13:11:47 -0700

    finally got it working

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    })
+        .then(r => r.json())
+        .then(email => {
+            const id = (email.id)
+            document.body.append(id)
+        })
+        .catch(error => {
+            document.body.append("request to http://localhost:3000/users failed, reason: Unauthorized Access")
+        })
+}
